735fab6b1cdd11859cd3dc3025316e52b2314329
calopsitaanderson <calopsitaanderson@gmail.com>
2020-02-07 22:30:20 -0300

    Aula de Java

---
+++ src/Nadador.java
@@ -0,0 +1,29 @@
+
+public class Nadador {
+	public static void main(String[] args) {
+		
+	int	idade = 3 ;
+	
+	if (idade >= 5 && idade  <= 7) {
+		System.out.println("infantil");
+	} 
+	else if (idade >= 8 && idade  <=10) {
+		System.out.println("juvenil");
+	} 
+	else if (idade >= 11 && idade  <= 15) {
+		System.out.println("adolescente ");
+	} 
+	else if (idade >=16 && idade <=30) {
+		System.out.println("adulto");
+	} 
+	else if  (idade > 30 ) {
+		System.out.println("senior");
+	} else {
+		System.out.println("menor que  5 anos ");
+
+	} 
+			
+		
+	}
+
+}
---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+
+		String nome = "anderson";
+		String sobrenome = "medeiros";
+		
+		
+		
+		/* System.out.println("ola," + nome +" "+ sobrenome);  */
+	
+		System.out.println(String.format(" ola , %s %s", nome, sobrenome));
+       
+       
+	}
+
+}
